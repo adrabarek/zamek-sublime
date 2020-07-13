2f7c75c124770e154161dcc8019684fe3d81be64
Andrzej Drabarek <andrzej.drabarek@protonmail.com>
2020-07-13 21:46:12 +0200

    Code cleanup.

--- zamek_sublime.py
+++ zamek_sublime.py
@@ -1,5 +1,3 @@
-import sublime
-import sublime_plugin
 import os
 import glob
 import pickle
@@ -7,6 +5,9 @@ import pprint
 import datetime
 
 from collections import namedtuple
+
+import sublime
+import sublime_plugin
 
 CONFIG_PATH = os.path.expanduser("~/.zamek")
 REGISTRY_PATH = os.path.join(CONFIG_PATH, "registry")
@@ -21,10 +22,7 @@ def note_name_from_path(path):
 
 
 def split_line(line):
-    return list(
-        filter(
-            lambda x: x != "", [
-                x.strip() for x in line.split(',')]))
+    return [x.strip() for x in line.split(',') if x != ""]
 
 
 def split_list(prefix, text_lines):
@@ -45,8 +43,8 @@ def note_from_path(path):
     tags = set([])
     if os.path.exists(path):
         lines = []
-        with open(path, "r") as file:
-            lines = file.readlines()
+        with open(path, "r") as infile:
+            lines = infile.readlines()
         links = set(split_list("links", lines))
         tags = set(split_list("tags", lines))
     else:
@@ -60,8 +58,8 @@ def note_from_path(path):
 def update_note_text(note, note_text=None):
     lines = []
     if not note_text:
-        with open(note.path, "r") as file:
-            lines = file.readlines()
+        with open(note.path, "r") as infile:
+            lines = infile.readlines()
     else:
         lines = note_text.split('\n')
 
@@ -81,21 +79,31 @@ def update_note_text(note, note_text=None):
                 datetime.datetime.now().strftime('%Y-%m-%d %H:%M') + '\n'
 
     if not note_text:
-        with open(note.path, "w") as file:
-            file.writelines(lines)
+        with open(note.path, "w") as outfile:
+            outfile.writelines(lines)
 
 
 def load_registry(path):
     try:
-        with open(path, 'rb') as file:
-            return pickle.load(file)
-    except Exception:
+        with open(path, 'rb') as infile:
+            return pickle.load(infile)
+    except OSError:
         return None
 
 
 def save_registry(registry, path):
-    with open(path, 'wb') as file:
-        pickle.dump(registry, file)
+    with open(path, 'wb') as outfile:
+        pickle.dump(registry, outfile)
+
+
+def add_link(first_note, second_note):
+    first_note.links.add(second_note.name)
+    second_note.links.add(first_note.name)
+
+
+def remove_link(first_note, second_note):
+    first_note.links.remove(second_note.name)
+    second_note.links.remove(first_note.name)
 
 
 class Registry:
@@ -106,8 +114,8 @@ class Registry:
 
     def add_note(self, note, note_text=None):
         if not note_text:
-            with open(note.path, 'r') as file:
-                note_text = file.read()
+            with open(note.path, 'r') as infile:
+                note_text = infile.read()
 
         text_lines = note_text.splitlines()
         tags = set(split_list("tags", text_lines))
@@ -120,7 +128,7 @@ class Registry:
         for linked_note_name in links:
             if self.__is_note_valid(linked_note_name):
                 other_note = self.notes[linked_note_name]
-                self.__add_link(note, other_note)
+                add_link(note, other_note)
                 update_note_text(other_note)
 
         update_note_text(note, note_text=note_text)
@@ -139,7 +147,7 @@ class Registry:
 
         links = note.links.copy()
         for link in links:
-            self.__remove_link(self.notes[link], note)
+            remove_link(self.notes[link], note)
             update_note_text(self.notes[link])
 
     def remove_deleted_links_and_tags(self, note):
@@ -158,7 +166,7 @@ class Registry:
             for deleted_link in deleted_links:
                 if self.__is_note_valid(deleted_link):
                     linked_note = self.notes[deleted_link]
-                    self.__remove_link(old_note, linked_note)
+                    remove_link(old_note, linked_note)
                     linked_note.update_text()
 
             update_note_text(note)
@@ -170,15 +178,7 @@ class Registry:
                 deleted_notes.append(self.notes[note_name])
 
         for note in deleted_notes:
-            self.clean_up_note(note)
-
-    def __add_link(self, first_note, second_note):
-        first_note.links.add(second_note.name)
-        second_note.links.add(first_note.name)
-
-    def __remove_link(self, first_note, second_note):
-        first_note.links.remove(second_note.name)
-        second_note.links.remove(first_note.name)
+            self.remove_note(note)
 
     def __is_note_valid(self, note_name):
         return note_name in self.notes and os.path.exists(
@@ -186,6 +186,7 @@ class Registry:
 
 
 class NoteSaver(sublime_plugin.EventListener):
+
     def on_pre_save(self, view):
         path = view.file_name()
         extension = os.path.splitext(path)[1]
@@ -194,7 +195,6 @@ class NoteSaver(sublime_plugin.EventListener):
             registry = load_registry(REGISTRY_PATH)
             note = note_from_path(path)
             registry.add_note(note, note_text)
-            registry.remove_deleted_links_and_tags(note)
 
             save_registry(registry, REGISTRY_PATH)
 
@@ -205,22 +205,27 @@ class NoteSaver(sublime_plugin.EventListener):
             note = note_from_path(path)
             update_note_text(note)
 
+            registry = load_registry(REGISTRY_PATH)
+            registry.remove_deleted_links_and_tags(note)
+
 
 class TagNoteInputHandler(sublime_plugin.ListInputHandler):
+
     def __init__(self, registry, tags):
         self.registry = registry
         self.tags = tags
 
     def list_items(self):
-        if self.tags.empty():
+        if not self.tags:
             return []
         chosen_notes = self.registry.tags[self.tags[0]]
-        for i in range(1, len(self.tags)):
-            chosen_notes = chosen_notes & self.registry.tags[self.tags[i]]
+        for _, tag in enumerate(self.tags):
+            chosen_notes = chosen_notes & self.registry.tags[tag]
         return list(chosen_notes)
 
 
 class TagNotesInputHandler(sublime_plugin.TextInputHandler):
+
     def __init__(self, registry):
         self.registry = registry
         self.tags = []
@@ -228,24 +233,26 @@ class TagNotesInputHandler(sublime_plugin.TextInputHandler):
     def confirm(self, text):
         self.tags = split_line(text)
 
-    def next_input(self, args):
+    def next_input(self, _):
         return TagNoteInputHandler(self.registry, self.tags)
 
 
 class LinkedNoteInputHandler(sublime_plugin.ListInputHandler):
+
     def __init__(self, command):
         self.command = command
 
     def list_items(self):
-        v = self.command.view
-        r = self.command.registry
-        if v.file_name() is None:
+        view = self.command.view
+        registry = self.command.registry
+        if view.file_name() is None:
             return []
-        note_name = note_name_from_path(v.file_name())
-        return list(r.notes[note_name].links)
+        note_name = note_name_from_path(view.file_name())
+        return list(registry.notes[note_name].links)
 
 
 class NoteNameInputHandler(sublime_plugin.ListInputHandler):
+
     def __init__(self, registry):
         self.registry = registry
 
@@ -254,7 +261,8 @@ class NoteNameInputHandler(sublime_plugin.ListInputHandler):
 
 
 class ZamekPrintRegistryCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+
+    def run(self, _):
         registry = load_registry(REGISTRY_PATH)
         if registry:
             pprint.pprint(vars(registry))
@@ -263,40 +271,54 @@ class ZamekPrintRegistryCommand(sublime_plugin.TextCommand):
                 "Registry file doesn't exist. "
                 "Use \"Zamek: Scan Directory For Notes\" to create it.")
 
-        print(note_name_from_path(self.view.file_name()))
-
 
 class ZamekOpenNoteCommand(sublime_plugin.TextCommand):
-    def run(self, edit, note_name):
+
+    def __init__(self, view):
+        self.registry = None
+        self.view = view
+
+    def run(self, _, note_name):
         if note_name:
             self.view.window().open_file(self.registry.notes[note_name].path)
 
-    def input(self, args):
+    def input(self, _):
         self.registry = load_registry(REGISTRY_PATH)
         return NoteNameInputHandler(self.registry)
 
 
 class ZamekOpenNoteWithTags(sublime_plugin.TextCommand):
+
+    def __init__(self, view):
+        self.registry = None
+        self.view = view
+
     def run(self, edit, tag_notes, tag_note):
         self.view.window().open_file(self.registry.notes[tag_note].path)
 
-    def input(self, args):
+    def input(self, _):
         self.registry = load_registry(REGISTRY_PATH)
         return TagNotesInputHandler(self.registry)
 
 
 class ZamekOpenLinkedNote(sublime_plugin.TextCommand):
-    def run(self, edit, linked_note):
-        if linked_note:
+
+    def __init__(self, view):
+        self.registry = None
+        self.view = view
+
+    def run(self, _, linked_note):
+        if linked_note and linked_note in self.registry.notes:
             self.view.window().open_file(self.registry.notes[linked_note].path)
 
-    def input(self, args):
+    def input(self, _):
         self.registry = load_registry(REGISTRY_PATH)
         return LinkedNoteInputHandler(self)
 
 
 class ZamekDeleteNote(sublime_plugin.TextCommand):
-    def run(self, edit):
+
+    def run(self, _):
         file_path = self.view.file_name()
         if file_path:
             (_, ext) = os.path.splitext(file_path)
@@ -314,7 +336,8 @@ class ZamekDeleteNote(sublime_plugin.TextCommand):
 
 
 class ZamekScanDirectoryForNotes(sublime_plugin.TextCommand):
-    def run(self, edit, text):
+
+    def run(self, _, text):
         if text:
             path = os.path.abspath(text)
             if not os.path.exists(path) or not os.path.isdir(path):
@@ -331,5 +354,5 @@ class ZamekScanDirectoryForNotes(sublime_plugin.TextCommand):
 
             save_registry(registry, REGISTRY_PATH)
 
-    def input(self, args):
+    def input(self, _):
         return sublime_plugin.TextInputHandler()
